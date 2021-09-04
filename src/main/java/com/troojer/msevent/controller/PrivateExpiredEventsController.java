143cb855cd02bf34c76dca1d364e0fa213fed5df
Emil <emilr@goldenpay.az>
2021-09-04 12:35:40 +0400

    - schedule moved to endpoint

--- src/main/java/com/troojer/msevent/controller/PrivateExpiredEventsController.java
+++ src/main/java/com/troojer/msevent/controller/PrivateExpiredEventsController.java
@@ -1,34 +1,38 @@
-package com.troojer.msevent.schedule;
+package com.troojer.msevent.controller;
 
 import ch.qos.logback.classic.Logger;
 import com.troojer.msevent.service.InnerEventService;
 import com.troojer.msevent.service.RandomEventService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@Component
-public class ExpiredEventsSchedule {
+@RestController
+@RequestMapping("private/events")
+@RequiredArgsConstructor
+public class PrivateExpiredEventsController {
 
     private final Logger logger = (Logger)LoggerFactory.getLogger(this.getClass());
 
     private final InnerEventService eventService;
     private final RandomEventService randomEventService;
 
-    public ExpiredEventsSchedule(InnerEventService eventService, RandomEventService randomEventService) {
-        this.eventService = eventService;
-        this.randomEventService = randomEventService;
-    }
-
-    @Scheduled(fixedDelay = 5 * 60_000L)
+    //fixedDelay = 5 * 60_000L
+    @GetMapping("close-expired")
     public void closeOldEvents() {
         eventService.setEndedStatusToAllExpired();
         logger.info("closeOldEvents() called");
     }
 
-    @Scheduled(fixedDelay = 30 * 24 * 60 * 60_000L)
+    //todo skoree vsego logika budet izmenena
+    //fixedDelay = 30 * 24 * 60 * 60_000L
+    @DeleteMapping("old-user-found-events")
     public void deleteOldUserFoundEvents() {
         randomEventService.deleteOld(LocalDateTime.now().minusMonths(24));
         logger.info("deleteOldUserFoundEvents() called");
