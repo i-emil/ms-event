6b169586448924ebf3b93bafc7b20b1c7c27058e
Emil <emilr@goldenpay.az>
2021-09-15 20:37:18 +0400

    - validation for filter dates added

--- src/main/java/com/troojer/msevent/model/FilterDto.java
+++ src/main/java/com/troojer/msevent/model/FilterDto.java
@@ -1,14 +1,16 @@
 package com.troojer.msevent.model;
 
+import com.troojer.msevent.constraints.FilterDateParameters;
 import com.troojer.msevent.model.enm.Gender;
+import com.troojer.msevent.model.label.FilterValidation;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Set;
 
 @Builder
@@ -18,15 +20,16 @@ import java.util.Set;
 public class FilterDto {
 
     @NotNull
-    //todo validation
+    @FilterDateParameters(groups = FilterValidation.class, param = "dates", message = "startDate must be less than endDate")
     private StartEndDatesDto dates;
 
     private AgeDto age;
 
     private Gender gender;
-    
+
     private Set<@Valid LanguageDto> languages;
 
+    @Positive
     private Long tagId;
 
     public void setProfileInfo(ProfileInfo profileInfo) {
